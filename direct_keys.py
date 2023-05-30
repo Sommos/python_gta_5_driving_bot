e8ef7beed2d6ba467c238e980b9ef69d1dc3a3dd
Sommos <Sommosgaming@gmail.com>
2023-05-30 18:14:03 +0100

    added direct_keys.py to simulate mouse and keyboard inputs

---
+++ direct_keys.py
@@ -0,0 +1,97 @@
+import ctypes
+import time
+
+# allows the program to send input using Windows API
+send_input = ctypes.windll.user32.SendInput
+
+# scan codes for W, A, S, D
+W = 0x11
+A = 0x1E
+S = 0x1F
+D = 0x20
+
+# C structure definitions 
+PUL = ctypes.POINTER(ctypes.c_ulong)
+
+class KeyBdInput(ctypes.Structure):
+                # wVk represents a virtual-key code
+    _fields_ = [("wVk", ctypes.c_ushort),
+                # wScan represents a hardware scan code
+                ("wScan", ctypes.c_ushort),
+                # dwFlags specifies various aspects of function operation
+                ("dwFlags", ctypes.c_ulong),
+                # time specifies the time stamp for the event, in milliseconds
+                ("time", ctypes.c_ulong),
+                # dwExtraInfo specifies extra information associated with the message
+                ("dwExtraInfo", PUL)]
+
+class HardwareInput(ctypes.Structure):
+                # uMsg specifies the message type
+    _fields_ = [("uMsg", ctypes.c_ulong),
+                # wParamL specifies the low-order word of the wParam argument
+                ("wParamL", ctypes.c_short),
+                # wParamH specifies the high-order word of the wParam argument
+                ("wParamH", ctypes.c_ushort)
+                ]
+
+class MouseInput(ctypes.Structure):
+                # dx specifies the mouse's absolute position along the x-axis or its amount of motion since the last mouse event was generated
+    _fields_ = [("dx", ctypes.c_long),
+                # dy specifies the mouse's absolute position along the y-axis or its amount of motion since the last mouse event was generated
+                ("dy", ctypes.c_long),
+                # mouseData specifies the mouse wheel's distance rotated, expressed in multiples or divisions of WHEEL_DELTA
+                ("mouseData", ctypes.c_ulong),
+                # dwFlags specifies various aspects of mouse motion and button clicks
+                ("dwFlags", ctypes.c_ulong),
+                # time specifies the time stamp for the event, in milliseconds
+                ("time",ctypes.c_ulong),
+                # dwExtraInfo specifies extra information associated with the message
+                ("dwExtraInfo", PUL)
+                ]
+
+class Input_I(ctypes.Union):
+                # the union of the three structures    
+    _fields_ = [("ki", KeyBdInput),
+                ("mi", MouseInput),
+                ("hi", HardwareInput)
+                ]
+
+class Input(ctypes.Structure):
+                # type specifies the type of the input event
+    _fields_ = [("type", ctypes.c_ulong),
+                # the union of the three structures
+                ("ii", Input_I)
+                ]
+
+# function to press key, hexKeyCode is the scan code of the key
+def PressKey(hexKeyCode):
+    # extra is a variable of type unsigned long equal to 0
+    extra = ctypes.c_ulong(0)
+    # ii_ is an instance of Input_I
+    ii_ = Input_I()
+    # ki is an instance of KeyBdInput, '0x0008' is the value of KEYEVENTF_EXTENDEDKEY to show an extended key event (it's been pressed down)
+    ii_.ki = KeyBdInput(0, hexKeyCode, 0x0008, 0, ctypes.pointer(extra))
+    # x is an instance of Input, '1' is the value of INPUT_KEYBOARD to show a keyboard event
+    x = Input( ctypes.c_ulong(1), ii_)
+    # send the input
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+# function to release key
+def ReleaseKey(hexKeyCode):
+    # extra is a variable of type unsigned long equal to 0
+    extra = ctypes.c_ulong(0)
+    # ii_ is an instance of Input_I
+    ii_ = Input_I()
+    # ki is an instance of KeyBdInput, '0x0008 | 0x0002' is the value of KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP to show an extended key event (it's been released)
+    ii_.ki = KeyBdInput(0, hexKeyCode, 0x0008 | 0x0002, 0, ctypes.pointer(extra))
+    # x is an instance of Input, '1' is the value of INPUT_KEYBOARD to show a keyboard event
+    x = Input( ctypes.c_ulong(1), ii_)
+    # send the input
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+if __name__ == '__main__':
+    while(True):
+        PressKey(0x11)
+        time.sleep(1)
+        ReleaseKey(0x11)
+        time.sleep(1)
--- main.py
+++ main.py
@@ -2,21 +2,17 @@ from PIL import ImageGrab
 import numpy as np
 import cv2
 import time
+from direct_keys import PressKey, ReleaseKey, W, A, S, D
 
-# sets the time at the start of the program
-last_time = time.time()
-while(True):
-    # 800 x 600 windowed mode
-    screen = np.array(ImageGrab.grab(bbox=(0, 40, 800, 640)))
-    
-    # print loop that prints the time each print screen takes and prints to console
-    print('loop took {} seconds'.format(time.time() - last_time))
-    last_time = time.time()
+# 4 seconds to switch to the game window
+for i in list(range(4))[:: -1]:
+    print(i + 1)
+    time.sleep(1)
 
-    # displays the image. and converts printscreen_pil to a numpy array from a PIL image
-    cv2.imshow('window', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
-
-    # if statement that breaks the loop if q is pressed
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        cv2.destroyAllWindows()
-        break
+def process_img(original_image):
+    # convert to gray from BGR
+    processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    # edge detection
+    processed_img = cv2.Canny(processed_img, threshold1 = 200, threshold2 = 300)
+    # return the processed image
+    return processed_img
