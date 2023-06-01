7d378310109f10edd4733fa8c811c413b4fc7437
Sommos <Sommosgaming@gmail.com>
2023-06-01 10:27:52 +0100

    added basic logic to get it to drive unaided

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from PIL import ImageGrab
+from statistics import mean
 from direct_keys import press_key, release_key, W, A, S, D
 import numpy as np
 import cv2
@@ -40,20 +41,20 @@ def draw_lanes(img, lines, color = [0, 255, 255], thickness = 3):
         line_dict = {}
 
         # loop through all the lines and find slope, intercept and coordinates of lines and add them to the arrays
-        for idx, i in enumerate(lines):
+        for index, i in enumerate(lines):
             for n in i:
                 # find the slope and intercept of the lines
                 x_coords = (n[0], n[2])
                 y_coords = (n[1], n[3])
                 # stacks the array vertically
                 A = np.vstack([x_coords, np.ones(len(x_coords))]).T
-                # find the slope and intercept
+                # save the slope and intercept
                 slope, y_intercept = np.linalg.lstsq(A, y_coords)[0]
                 # find the coordinates of the lines
                 x1 = (min_y - y_intercept) / slope
                 x2 = (max_y - y_intercept) / slope
                 # add the slope, intercept and coordinates of the lines to the array
-                line_dict[idx] = [slope, y_intercept, [int(x1), min_y, int(x2), max_y]]
+                line_dict[index] = [slope, y_intercept, [int(x1), min_y, int(x2), max_y]]
                 # add the coordinates of the lines to the array
                 new_lines.append([int(x1), min_y, int(x2), max_y])
         
@@ -61,15 +62,15 @@ def draw_lanes(img, lines, color = [0, 255, 255], thickness = 3):
         final_lanes = {}
 
         # loop through all the lines and find slope, intercept and coordinates of lines and add them to the arrays    
-        for idx in line_dict:
+        for index in line_dict:
             # create a copy of the final_lanes array
             final_lanes_copy = final_lanes.copy()
             # store the slope
-            slope = line_dict[idx][0]
+            slope = line_dict[index][0]
             # store the intercept
-            y_intercept = line_dict[idx][1]
+            y_intercept = line_dict[index][1]
             # store the coordinates of the lines
-            line = line_dict[idx][2]
+            line = line_dict[index][2]
 
             # if the final_lanes array is empty, add the slope, intercept and coordinates of the lines to the array
             if len(final_lanes) == 0:
@@ -81,6 +82,7 @@ def draw_lanes(img, lines, color = [0, 255, 255], thickness = 3):
                     # if the slope is similar to the slope in the final_lanes array, add the slope, intercept and coordinates of the lines to the array
                     if not found_copy:
                         if abs(other_ms * 1.2) > abs(slope) > abs(other_ms * 0.8):
+                            # loop until the slope is similar to the slope in the final_lanes array
                             if abs(final_lanes_copy[other_ms][0][1] * 1.2) > abs(y_intercept) > abs(final_lanes_copy[other_ms][0][1] * 0.8):
                                 final_lanes[other_ms].append([slope, y_intercept, line])
                                 found_copy = True
@@ -113,15 +115,15 @@ def draw_lanes(img, lines, color = [0, 255, 255], thickness = 3):
                 y1s.append(data[2][1])
                 x2s.append(data[2][2])
                 y2s.append(data[2][3])
-            # find the average of the coordinates, and convert them to integers before returning
-            return int(np.mean(x1s)), int(np.mean(y1s)), int(np.mean(x2s)), int(np.mean(y2s))
+            # find the mean of the coordinates, and convert them to integers before returning
+            return int(mean(x1s)), int(mean(y1s)), int(mean(x2s)), int(mean(y2s))
         
         # find the average coordinates of the lines, and save them in the variables
         l1_x1, l1_y1, l1_x2, l1_y2 = average_lane(final_lanes[lane1_id])
         l2_x1, l2_y1, l2_x2, l2_y2 = average_lane(final_lanes[lane2_id])
 
         # draw the lines on the image
-        return [[l1_x1, l1_y1, l1_x2, l1_y2], [l2_x1, l2_y1, l2_x2, l2_y2]]
+        return [l1_x1, l1_y1, l1_x2, l1_y2], [l2_x1, l2_y1, l2_x2, l2_y2], lane1_id, lane2_id
     # if no lines are detected, return the error message
     except Exception as e:
         print(str(e))
@@ -133,8 +135,8 @@ def process_img(image):
     processed_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # edge detection
     processed_img = cv2.Canny(processed_img, threshold1 = 200, threshold2 = 300)
-    # # gaussian blur the image
-    # processed_img = cv2.GaussianBlur(processed_img, (5, 5), 0)
+    # gaussian blur the image
+    processed_img = cv2.GaussianBlur(processed_img, (5, 5), 0)
     # vertices of the polygon
     vertices = np.array([
                         [10, 500], 
@@ -149,9 +151,12 @@ def process_img(image):
     # call the HoughLinesP function over the processed image
     # cv2.HoughLinesP(image, rho, thetha, threshold, lines, minLineLength, maxLineGap)
     lines = cv2.HoughLinesP(processed_img, 1, np.pi/180, 180, np.array([]), 20, 15)
+    # instantiate the slope variables
+    slope1 = 0
+    slope2 = 0
     try:
-        # call the draw_lanes function over the original image
-        l1, l2 = draw_lanes(original_image, lines)
+        # call the draw_lanes function over the original image, and store the returned values
+        l1, l2, slope1, slope2 = draw_lanes(original_image, lines)
         # draw the lines on the original image
         cv2.line(original_image, (l1[0], l1[1]), (l1[2], l1[3]), [0, 255, 0], 30)
         cv2.line(original_image, (l2[0], l2[1]), (l2[2], l2[3]), [0, 255, 0], 30)
@@ -159,7 +164,6 @@ def process_img(image):
     except Exception as e:
         print(str(e))
         pass
-    
     try:
         for coords in lines:
             coords = coords[0]
@@ -171,8 +175,36 @@ def process_img(image):
                 print(str(e))
     except Exception as e:
         pass
-    # return the processed image and the original image
-    return processed_img, original_image
+    # return the processed image, original image and both slope variables
+    return processed_img, original_image, slope1, slope2
+
+# functions to press the keys
+def straight():
+    press_key(W)
+    release_key(A)
+    release_key(D)
+
+def left():
+    press_key(A)
+    release_key(W)
+    release_key(D)
+    release_key(A)
+
+def right():
+    press_key(D)
+    release_key(A)
+    release_key(W)
+    release_key(D)
+
+def slow():
+    release_key(W)
+    release_key(A)
+    release_key(D)
+
+# countdown timer before it starts
+for i in list(range(4))[::-1]:
+    print(i+1)
+    time.sleep(1)
 
 def main():
     last_time = time.time()
@@ -182,11 +214,19 @@ def main():
         # print loop that prints the time each print screen takes and prints to console
         print('Loop took {} seconds'.format(time.time() - last_time))
         last_time = time.time()
-        new_screen, original_image = process_img(screen)
-        # displays the processed image
-        cv2.imshow('window', new_screen)
+        new_screen, original_image, slope1, slope2 = process_img(screen)
+        # # displays the processed image
+        # cv2.imshow('window', new_screen)
         # displays the original image
         cv2.imshow('window2', cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
+
+        # if statements that call the functions to press the keys depending on the value of slope1 and slope2
+        if slope1 < 0 and slope2 < 0:
+            right()
+        elif slope1 > 0 and slope2 > 0:
+            left()
+        else:
+            straight()
 
         # if statement that breaks the loop if q is pressed for 25 milliseconds
         if cv2.waitKey(25) & 0xFF == ord('q'):
